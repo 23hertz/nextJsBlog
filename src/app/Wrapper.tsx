f526361570f16cbc7d486d8a1ada402cc1b2f1e5
Shakiru <Olaiya.adeolu@gmail.com>
2025-09-15 07:44:00 +0100

    fixing of

--- src/app/Wrapper.tsx
+++ src/app/Wrapper.tsx
@@ -7,7 +7,7 @@ import Nav from "./Nav";
 import { useState } from "react";
 const Wrapper = () => {
   const [searchQuery, setSearchQuery] = useState("");
-  const [menuOpen, setMenuOpen] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(true);
 
   return (
     <div className="h-auto sticky top-0 z-30">
@@ -19,11 +19,18 @@ const Wrapper = () => {
       />
 
       <div className="flex min-h-screen">
-        
-        <div
+        {/* <div
           className={`fixed lg:static top-0 left-0 h-full bg-white shadow-lg z-20 transform transition-transform duration-300 
           ${menuOpen ? "translate-x-0 w-[60%] mt-[110px]" : "-translate-x-full"} 
           lg:translate-x-0 lg:w-[15%] lg:mt-0`}
+        >
+          <Nav menuOpen={menuOpen} />
+        </div> */}
+
+        <div
+          className={`fixed lg:static top-[72px] lg:top-0 left-0 h-full bg-white shadow-lg z-20 transform transition-transform duration-300
+    ${menuOpen ? "translate-x-0 w-[60%]" : "-translate-x-full"} 
+    lg:translate-x-0 lg:w-[15%]`}
         >
           <Nav menuOpen={menuOpen} />
         </div>
